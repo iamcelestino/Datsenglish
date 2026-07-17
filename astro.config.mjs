a148ba9620b3ad498675dcb0d9c62ffecf1bcd9a
iamcelestino <trcelestino488@gmail.com>
2026-07-16 19:45:38 -0500

    feat: add minutesRead to blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-// @ts-check
+import { unified } from '@astrojs/markdown-remark';
 import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
-// https://astro.build/config
 export default defineConfig({
+	markdown: {
+	    processor: unified({
+	      remarkPlugins: [remarkReadingTime],
+	    }),
+	},
 	i18n: {
 	    locales: ["en", "pt"],
 	    defaultLocale: "en",
